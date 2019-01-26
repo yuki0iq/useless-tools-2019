69c408ba1d0a2c4470dd582d2df7bd332b959131
ADMIN <noemail>
2019-01-26 16:38:06 +0300

    add grid and squared algebra

--- UselessTools/PlotWnd.cpp
+++ UselessTools/PlotWnd.cpp
@@ -1,4 +1,14 @@
 #include "PlotWnd.h"
+#include <stdio.h>
+#include <math.h>
+
+void Marker(LONG x, LONG y, HDC hDC)
+{
+	MoveToEx(hDC, (int)x - 10, (int)y, (LPPOINT)NULL);
+	LineTo(hDC, (int)x + 10, (int)y);
+	MoveToEx(hDC, (int)x, (int)y - 10, (LPPOINT)NULL);
+	LineTo(hDC, (int)x, (int)y + 10);
+}
 
 CPlotWnd* CPlotWnd::pWnd = 0;
 
@@ -6,12 +16,13 @@ CPlotWnd::CPlotWnd()
 {
 }
 
-void CPlotWnd::Show(HINSTANCE hInst, LPCSTR pchTitle, int w, int h, Func func, double Xm, double Ym, double Xs, double Ys)
+void CPlotWnd::Show(HINSTANCE hInst, LPCSTR pchTitle, int w, int h, Func func, double Xm, double Ym, double Xs, double Ys, char *pInfo)
 {
 	m_hInst = hInst;
 	m_Func = func;
 	m_Xm = Xm, m_Ym = Ym;
 	m_Xs = Xs, m_Ys = Ys;
+	m_pInfo = pInfo;
 
 	// Register the window class.
 	const char CLASS_NAME[] = "UselesTools.PlotWnd";
@@ -66,27 +77,98 @@ void CPlotWnd::DoPaint(HDC hDC)
 	GetClientRect(m_hWnd, &rect);
 	FillRect(hDC, &rect, (HBRUSH)(COLOR_WINDOW + 1));
 
-	LPCSTR kkk = "Построение графика";
-	TextOut(hDC, 20, 20, kkk, strlen(kkk));
-
 	LONG w = rect.right - rect.left;
 	LONG h = rect.bottom - rect.top;
 
-	//AXES
 	LONG w_mid = w / 2;
 	LONG h_mid = h / 2;
-	MoveToEx(hDC, w_mid, 0, NULL);
-	LineTo(hDC, w_mid, h);
-	TextOut(hDC, w_mid + 4, 4, "Y", 1);
-	MoveToEx(hDC, 0, h_mid, NULL);
-	LineTo(hDC, w, h_mid);
-	TextOut(hDC, w - 15, h_mid + 4, "X", 1);
 
 	//GRID
+	HPEN hPen = CreatePen(PS_SOLID, 1, RGB(192, 192, 192));
+	HGDIOBJ hOldPen = SelectObject(hDC, hPen);
+	{
+		int hor = 0;
+		while (hor < h_mid)
+		{
+			hor += PLOT_UNIT;
+			MoveToEx(hDC, 0, h_mid + hor, NULL);
+			LineTo(hDC, m_wndW, h_mid + hor);
+			MoveToEx(hDC, 0, h_mid - hor, NULL);
+			LineTo(hDC, m_wndW, h_mid - hor);
+		}
+		int ver = 0;
+		while (ver < w_mid)
+		{
+			ver += PLOT_UNIT;
+			MoveToEx(hDC, 0, w_mid + ver, NULL);
+			LineTo(hDC, m_wndH, w_mid + ver);
+			MoveToEx(hDC, 0, w_mid - ver, NULL);
+			LineTo(hDC, m_wndH, w_mid - ver);
+		}
+	}
+	SelectObject(hDC, hOldPen);
+	DeleteObject(hPen);
 
+	//AXES
+	char kkk[1000] = { "\0" };
+	sprintf(kkk, "O(%g;%g), X=%g, Y=%g", m_Xm, m_Ym, m_Xs, m_Ys);
+	TextOut(hDC, 10, 10, kkk, strlen(kkk));
+	TextOut(hDC, 10, 25, m_pInfo, strlen(m_pInfo));
+
+	hPen = CreatePen(PS_SOLID, 2, RGB(0, 0, 0));
+	hOldPen = SelectObject(hDC, hPen);
+	{
+		MoveToEx(hDC, w_mid, 0, NULL);
+		LineTo(hDC, w_mid, h);
+		TextOut(hDC, w_mid + 4, 4, "Y", 1);
+
+		MoveToEx(hDC, 0, h_mid, NULL);
+		LineTo(hDC, w, h_mid);
+		TextOut(hDC, w - 15, h_mid + 4, "X", 1);
+
+		MoveToEx(hDC, w_mid + 20, h_mid + 5, NULL);
+		LineTo(hDC, w_mid + PLOT_UNIT, h_mid - PLOT_ZASECKA);
+
+		MoveToEx(hDC, w_mid - 5, h_mid - 20, NULL);
+		LineTo(hDC, w_mid + PLOT_ZASECKA, h_mid - PLOT_UNIT);
+	}
+	SelectObject(hDC, hOldPen);
+	DeleteObject(hPen);
+
+	//NULL-POINT
+	hPen = CreatePen(PS_SOLID, 2, RGB(0, 0, 255));
+	hOldPen = SelectObject(hDC, hPen);
+	{
+		Marker(REALtoPX_X(0), REALtoPX_Y(0), hDC);
+	}
+	SelectObject(hDC, hOldPen);
+	DeleteObject(hPen);
 
 	//FUNC
-	double y = m_Func(0);
+	hPen = CreatePen(PS_SOLID, 2, RGB(255, 0, 0));
+	hOldPen = SelectObject(hDC, hPen);
+	{
+		MoveToEx(hDC, -m_wndW, h_mid, NULL);
+		for (int x_p = 0; x_p < m_wndW; ++x_p)
+		{
+			double x_u = PXtoREAL_X(x_p);
+			double y_u = m_Func(x_u);
+			int y_p = REALtoPX_Y(y_u);
+			LineTo(hDC, x_p, y_p);
+			MoveToEx(hDC, x_p, y_p, NULL);
+		}
+	}
+	SelectObject(hDC, hOldPen);
+	DeleteObject(hPen);
+}
+
+void CPlotWnd::DoSize()
+{
+	RECT aaa;
+	GetClientRect(m_hWnd, &aaa);
+	m_wndH = aaa.bottom - aaa.top;
+	m_wndW = aaa.right - aaa.left;
+	InvalidateRect(m_hWnd, NULL, false);
 }
 
 LRESULT CPlotWnd::WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -109,12 +191,36 @@ LRESULT CPlotWnd::WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			}
 			return 0;
 
-		/*case WM_ERASEBKGND:
-			return 1;*/
-
 		case WM_SIZE:
-			InvalidateRect(hWnd, NULL, false);
+			if (CPlotWnd::pWnd != 0)
+			{
+				pWnd->DoSize();
+			}
 			break;
 	}
 	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+int CPlotWnd::REALtoPX_X(double x)
+{
+	double dxu = x - m_Xm;
+	double wu = m_wndW / (double)PLOT_UNIT;
+	double dpixel = m_wndW * dxu / wu;
+	int pos = (int)round(dpixel) + m_wndW/2;
+	return pos;
+}
+
+int CPlotWnd::REALtoPX_Y(double y)
+{
+	double dyu = y - m_Ym;
+	double hu = m_wndH / (double)PLOT_UNIT;
+	double dpixel = (double)m_wndH * dyu / hu;
+	int pos = m_wndH / 2 - (int)round(dpixel);
+	return pos;
+}
+
+double CPlotWnd::PXtoREAL_X(int x)
+{
+	double dxu = ((double)x - m_wndW/2.) / (double)PLOT_UNIT + m_Xm;
+	return dxu;
 }
--- UselessTools/PlotWnd.h
+++ UselessTools/PlotWnd.h
@@ -5,24 +5,33 @@
 //y = Func(x)
 typedef double(*Func)(double);
 
+#define PLOT_UNIT 20
+#define PLOT_ZASECKA 5
+
 class CPlotWnd
 {
-	HWND      m_hWnd;
-	HINSTANCE m_hInst;
-	Func      m_Func;
-	double    m_Xm, m_Ym;
-	double    m_Xs, m_Ys;
+	HWND      m_hWnd = 0;
+	HINSTANCE m_hInst = 0;
+	Func      m_Func = 0;
+	double    m_Xm = 0, m_Ym = 0;
+	double    m_Xs = 0, m_Ys = 0;
+	int       m_wndW = 0, m_wndH = 0;
+	char      *m_pInfo = 0;
 
 public:
 	CPlotWnd();
-	void Show(HINSTANCE hInst, LPCSTR pchTitle, int w, int h, Func func, double Xm, double Ym, double Xs, double Ys);
+	void Show(HINSTANCE hInst, LPCSTR pchTitle, int w, int h, Func func, double Xm, double Ym, double Xs, double Ys, char *pInfo);
 	~CPlotWnd();
 
 	void DoPaint(HDC hDC);
+	void DoSize();
 
 	static CPlotWnd* pWnd;
 
 private:
 	static LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	int REALtoPX_X(double x);
+	int REALtoPX_Y(double y);
+	double PXtoREAL_X(int x);
 };
 
